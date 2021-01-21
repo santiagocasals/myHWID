b6544dba220e0c5b83d818308eb8ad577c1db538
Santiago Casals <santiago.casals@outlook.com>
2021-01-21 12:14:02 -0300

    main

--- pw/santiago/Main.java
+++ pw/santiago/Main.java
@@ -14,7 +14,7 @@ public class Main {
             MessageDigest myHWID = MessageDigest.getInstance("MD5");
             return myHWID.digest(Utils.all_identifiers.getBytes());
         } catch (Exception e) {
-            System.out.println("[myHWID] A error has been generated, please report:");
+            System.out.println("[myHWID] A error has been generated, please report: ");
             System.out.println(e);
         }
 		return null;
